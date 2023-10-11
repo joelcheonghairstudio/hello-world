d99a7681d2eec48b72720cdc2d9250d0ea09088a
joelcheonghairstudio <joelcheonghairstudio@gmail.com>
2023-10-11 14:31:14 +0800

    Update general.js

--- pos/general.js
+++ pos/general.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('.menu_import')
 fetch("/menu.html")
-.then(res=>res.text())
-.then(data=>{
-    nav.innerHTML=data
-})
+    .then(res => res.text())
+    .then(data => {
+        nav.innerHTML = data
+    })
